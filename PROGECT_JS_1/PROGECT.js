2c863acd6b7b6d79926d7d889720d05f0846d62e
Mondel <jassmina_121@mail.ru>
2020-03-10 23:32:54 +0300

    Проект Черновик

---
+++ PROGECT_JS_1/PROGECT.js
@@ -0,0 +1,208 @@
+let obj_1 = //[
+	{
+		name: "capuchino",
+		photo: 'url',
+		ingredients: [
+			{
+				name: 'sugar',
+				photo: 'url',
+				quantity: 10
+			}, {
+				name: 'cofee',
+				photo: 'url',
+				quantity: 60
+			},{
+				name: 'milk',
+				photo: 'url',
+				quantity: 80
+			}
+			
+		],
+		size: 250,
+	    recipe: 1
+	}
+	 console.log(obj_1);
+//] 
+
+let obj_2 = //[
+	{
+		name: "americano",
+		photo: 'url',
+		ingredients: [
+			{
+				name: 'sugar',
+				photo: 'url',
+				quantity: 10
+			}, {
+				name: 'ground coffee',//молотый
+				photo: 'url',
+				quantity: 60
+			},{
+				name: 'water',
+				photo: 'url',
+				quantity: 30
+			}
+			
+		],
+		size: 180,
+	    recipe: 1
+	}
+//]
+
+let obj_3 = //[
+	{
+		name: "latte",
+		photo: 'url',
+		ingredients: [
+			{
+				name: 'sugar',
+				photo: 'url',
+				quantity: 10
+			}, {
+				name: 'coffee',//молотый
+				photo: 'url',
+				quantity: 30
+			},{
+				name: 'milk',
+				photo: 'url',
+				quantity: 150
+			}
+			
+		],
+		size: 220,
+	    recipe: 1
+	}
+//]
+
+let obj_4 = //[
+	{
+		name: "flat_white",
+		photo: 'url',
+		ingredients: [
+			{
+				name: 'sugar',
+				photo: 'url',
+				quantity: 10
+			}, {
+				name: 'espresso',//молотый
+				photo: 'url',
+				quantity: 80
+			},{
+				name: 'whipped milk', //взбитое молоко
+				photo: 'url',
+				quantity: 150
+			}
+			
+		],
+		size: 230,
+	    recipe: 1
+	}
+//]
+
+let obj_5 = //[
+	{
+		name: "mochaccino",
+		photo: 'url',
+		ingredients: [
+			{
+				name: 'sugar',
+				photo: 'url',
+				quantity: 10
+			}, {
+				name: 'espresso',//молотый
+				photo: 'url',
+				quantity: 10
+			},{
+				name: "whipping cream",
+				photo: 'url',
+				quantity: 50
+			},{
+				name: "cooled milk",
+				photo: 'url',
+				quantity: 40
+			},{
+				name: "dark chocolate",
+				photo: 'url',
+				quantity: 50
+			}
+		],
+		size: 320,
+	    recipe: 1
+	}
+//]
+
+let obj_6 = //[
+	{
+		name: "espresso",
+		photo: 'url',
+		ingredients: [
+			{
+				name: 'sugar',
+				photo: 'url',
+				quantity: 10
+			}, {
+				name: 'ground coffee',//молотый
+				photo: 'url',
+				quantity: 10
+			},{
+				name: "coarse salt",//крупная соль
+				photo: 'url',
+				quantity: 3
+			}
+		],
+		size: 320,
+	    recipe: 1
+	}
+//]
+
+let obj_7 = //[
+	{
+		name: "raf",
+		photo: 'url',
+		ingredients: [
+			{
+				name: 'sugar',
+				photo: 'url',
+				quantity: 10
+			}, {
+				name: 'espresso',//молотый
+				photo: 'url',
+				quantity: 25
+			},{
+				name: "vanilla sugar",//крупная соль
+				photo: 'url',
+				quantity: 5
+			},{
+				name: "cream",//крупная соль
+				photo: 'url',
+				quantity: 100
+			}
+		],
+		size: 280,
+	    recipe: 1
+	}
+//]
+
+let obj_8 = //[
+	{
+		name: "lungo",
+		photo: 'url',
+		ingredients: [
+			{
+				name: 'sugar',
+				photo: 'url',
+				quantity: 10
+			}, {
+				name: 'rabusta',//молотый
+				photo: 'url',
+				quantity: 2
+			},{
+				name: "water",//крупная соль
+				photo: 'url',
+				quantity: 50
+			}
+		],
+		size: 150,
+	    recipe: 1
+	}
+//]
